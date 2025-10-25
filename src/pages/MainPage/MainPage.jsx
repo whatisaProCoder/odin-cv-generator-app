a9cd64395e308e249cd74767430e49eea92f873d
whatisaProCoder <debnathpritam2005@gmail.com>
2025-10-25 18:46:36 +0530

    made prof exp section as separate component

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -13,6 +13,7 @@ import { useMediaQuery } from "react-responsive";
 import ChooseTemplateSection from "./ChooseTemplateSection";
 import PersonalInfoSection from "./PersonalInfoSection";
 import SkillsSection from "./SkillsSection";
+import ProfessionalExpSection from "./ProfessionalExpSection";
 
 function MainPage() {
   const isTablet = useMediaQuery({ query: "(max-width: 768px)" });
@@ -59,25 +60,6 @@ function MainPage() {
     { point: "", details: "" },
     { point: "", details: "" },
   ]);
-
-  const handleAddSkills = () => {
-    const newSkills = [...skills];
-    newSkills.push("");
-    setSkills(newSkills);
-  };
-
-  const handleAddProfessionalExperience = () => {
-    const newProfessionalExperiences = [...professionalExperiences];
-    newProfessionalExperiences.push({
-      projectOrRole: "",
-      company: "",
-      startDate: "",
-      endDate: "",
-      workingAtPresent: false,
-      workingDetails: ["", "", ""],
-    });
-    setProfessionalExperiences(newProfessionalExperiences);
-  };
 
   const handleAddEducationExperience = () => {
     const newEducationExperiences = [...educationExperiences];
@@ -129,204 +111,11 @@ function MainPage() {
             setPersonalInfo={setPersonalInfo}
           />
           <SkillsSection skills={skills} setSkills={setSkills} />
-          <div className="mt-12 flex flex-row items-center mb-4">
-            <div className="font-semibold text-[0.9rem] flex-1">
-              Professional Experience
-            </div>
-            <IconButton
-              onClick={handleAddProfessionalExperience}
-              iconSrc={addIcon}
-              text="ADD"
-            />
-          </div>
-          <div className="mt-4 flex flex-col gap-6">
-            {professionalExperiences.map((experience, index) => {
-              return (
-                <div key={"profexp" + index} className="flex flex-col gap-4">
-                  <div className="text-[0.85rem] font-semibold">
-                    {"Experience " + (index + 1)}
-                  </div>
-                  <div className="flex flex-row gap-4 max-md:flex-col">
-                    <InputField
-                      id="project-role"
-                      value={experience.projectOrRole}
-                      placeholder="Project / Role"
-                      onChange={(e) => {
-                        const newProfessionalExperiences = [
-                          ...professionalExperiences,
-                        ];
-                        newProfessionalExperiences[index].projectOrRole =
-                          e.target.value;
-                        setProfessionalExperiences(newProfessionalExperiences);
-                      }}
-                      style={{ flex: "1" }}
-                    />
-                    <InputField
-                      id="company"
-                      value={experience.company}
-                      placeholder="Company"
-                      onChange={(e) => {
-                        const newProfessionalExperiences = [
-                          ...professionalExperiences,
-                        ];
-                        newProfessionalExperiences[index].company =
-                          e.target.value;
-                        setProfessionalExperiences(newProfessionalExperiences);
-                      }}
-                      style={{ flex: "1" }}
-                    />
-                  </div>
-                  <div className="flex flex-row items-center gap-4 max-md:flex-col">
-                    <div className="flex flex-row items-center gap-4 max-md:w-full">
-                      <div className="text-[0.8rem] font-medium">
-                        Start Date
-                      </div>
-                      <InputField
-                        id="start-date"
-                        type="month"
-                        value={experience.startDate}
-                        onChange={(e) => {
-                          const newProfessionalExperiences = [
-                            ...professionalExperiences,
-                          ];
-                          newProfessionalExperiences[index].startDate =
-                            e.target.value;
-                          setProfessionalExperiences(
-                            newProfessionalExperiences
-                          );
-                        }}
-                        style={{ flex: "1" }}
-                      />
-                    </div>
-                    <div className="flex flex-row items-center gap-4 max-md:w-full">
-                      <div className="text-[0.8rem] font-medium max-md:mr-[0.4rem]">
-                        End Date
-                      </div>
-                      <InputField
-                        id="end-date"
-                        type="month"
-                        value={experience.endDate}
-                        onChange={(e) => {
-                          const newProfessionalExperiences = [
-                            ...professionalExperiences,
-                          ];
-                          newProfessionalExperiences[index].endDate =
-                            e.target.value;
-                          setProfessionalExperiences(
-                            newProfessionalExperiences
-                          );
-                        }}
-                        style={{ flex: "1" }}
-                        disabled={experience.workingAtPresent ? true : false}
-                      />
-                    </div>
-                  </div>
-                  <div className="flex flex-row gap-3">
-                    <div className="text-[0.8rem] font-medium">
-                      Working at present?
-                    </div>
-                    <input
-                      type="checkbox"
-                      style={{
-                        accentColor: AccentColors.find(
-                          (accentColor) => accentColor.id === accentColorID
-                        ).color,
-                        transform: "scale(1.25)",
-                      }}
-                      onChange={(e) => {
-                        const newProfessionalExperiences = [
-                          ...professionalExperiences,
-                        ];
-                        newProfessionalExperiences[index].workingAtPresent =
-                          e.target.checked;
-                        setProfessionalExperiences(newProfessionalExperiences);
-                      }}
-                    />
-                  </div>
-                  <div className="flex flex-row items-center">
-                    <div className="flex-1 text-[0.8rem] font-medium">
-                      Working Details / Achievements
-                    </div>
-                    <IconButton
-                      onClick={() => {
-                        const newProfessionalExperiences = [
-                          ...professionalExperiences,
-                        ];
-                        newProfessionalExperiences[index].workingDetails.push(
-                          ""
-                        );
-                        setProfessionalExperiences(newProfessionalExperiences);
-                      }}
-                      iconSrc={addIcon}
-                      text="New Point"
-                    />
-                  </div>
-                  <div className="flex flex-col gap-4">
-                    {experience.workingDetails.map((point, innerIndex) => {
-                      return (
-                        <div
-                          key={"workingdetail" + index + innerIndex}
-                          className="flex flex-row items-center"
-                        >
-                          <InputField
-                            key={"workingdetail" + index + innerIndex}
-                            id={"workingdetail" + index + innerIndex}
-                            value={point}
-                            placeholder={"Achievement " + (innerIndex + 1)}
-                            onChange={(e) => {
-                              const newProfessionalExperiences = [
-                                ...professionalExperiences,
-                              ];
-                              newProfessionalExperiences[index].workingDetails[
-                                innerIndex
-                              ] = e.target.value;
-                              setProfessionalExperiences(
-                                newProfessionalExperiences
-                              );
-                            }}
-                            style={{ flex: "1" }}
-                          />
-                          <button
-                            onClick={() => {
-                              let newProfessionalExperiences = [
-                                ...professionalExperiences,
-                              ];
-                              newProfessionalExperiences[index].workingDetails =
-                                [
-                                  ...newProfessionalExperiences[
-                                    index
-                                  ].workingDetails.slice(0, innerIndex),
-                                  ...newProfessionalExperiences[
-                                    index
-                                  ].workingDetails.slice(innerIndex + 1),
-                                ];
-                              if (
-                                newProfessionalExperiences[index].workingDetails
-                                  .length === 0
-                              ) {
-                                newProfessionalExperiences[
-                                  index
-                                ].workingDetails = ["", "", ""];
-                              }
-                              setProfessionalExperiences(
-                                newProfessionalExperiences
-                              );
-                            }}
-                          >
-                            <img
-                              src={closeIcon}
-                              alt="Close Button"
-                              className="h-6 w-6 ml-3 transition-transform hover:scale-115"
-                            />
-                          </button>
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+          <ProfessionalExpSection
+            accentColorID={accentColorID}
+            professionalExperiences={professionalExperiences}
+            setProfessionalExperiences={setProfessionalExperiences}
+          />
           <div className="mt-12 flex flex-row items-center mb-4">
             <div className="font-semibold text-[0.9rem] flex-1">Education</div>
             <IconButton
