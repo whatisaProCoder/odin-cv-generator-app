6baaf244c93e9988aaaf8dcf8c4241e87aa852f2
whatisaProCoder <debnathpritam2005@gmail.com>
2025-10-28 14:05:43 +0530

    added accent colors

--- src/constants/accentColors.js
+++ src/constants/accentColors.js
@@ -1,7 +1,9 @@
 export const AccentColors = [
-  { id: 0, color: "#2263C8" },
-  { id: 1, color: "#FE3D3D" },
-  { id: 2, color: "#6F00FF" },
-  { id: 3, color: "#D86500" },
-  { id: 4, color: "#141414" },
+  { id: 0, color: "#2263C8" }, // Blue
+  { id: 4, color: "#6F00FF" }, // Violet
+  { id: 3, color: "#00B012" }, // Green
+  { id: 5, color: "#009E66" }, // Green (darker/teal)
+  { id: 2, color: "#D86500" }, // Orange
+  { id: 1, color: "#FE3D3D" }, // Red
+  { id: 6, color: "#141414" }  // Black 
 ]
